0de3b8eb72a9f074d9f95a4569a4ec766302fcb5
NikitaDe <69841196+NikitaDe@users.noreply.github.com>
2020-12-02 18:48:07 +0300

    debug logout

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -50,9 +50,10 @@ function Navigations() {
   const logout = async () => {
     await axios.get('https://focus-network.herokuapp.com/logout').then(() => {
       // localStorage.removeItem('token');
-      console.log(localStorage.getItem('token'));
-      window.localStorage.clear();
-      window.location.reload();
+      // console.log(localStorage.getItem('token'));
+      // window.localStorage.clear();
+      // window.location.reload();
+      console.log('logout');
     });
   };
   const [theme, setTheme] = React.useState(true);
